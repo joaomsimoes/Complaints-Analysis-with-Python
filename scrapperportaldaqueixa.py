c07f982165ee7cb7cac6c274e7c9e518982c4dbe
vascosimoes <vascomms1@hotmail.com>
2021-07-06 13:00:52 +0100

    removed global variables and recursive function

--- scrapperportaldaqueixa.py
+++ scrapperportaldaqueixa.py
@@ -5,8 +5,6 @@ import re
 import sys
 sys.setrecursionlimit(10000)
 
-pages = set()
-dictf = {}
 
 def getLinks(brand):
   ''' Portal da Queixa WebScrapper. It scrappes only the ../{brand}/complaint/* pages
@@ -15,28 +13,12 @@ def getLinks(brand):
   Return:
     CSV file with the name of the brand and for each row complain title and its text'''
   
-  global pages, dictf
-
+  pages = set()
+  complaints = []
+  
   # open the html and start the soup 
   html = urlopen('https://portaldaqueixa.com/brands/{}/complaints/'.format(brand))
   bs = BeautifulSoup(html, 'html.parser')
-
-  # get the title and complaint text from the page 
-  lenght_brand = len(str(brand))
-
-  brand_title_find = bs.find('h4')
-  brand_title_text = brand_title_find.get_text()
-
-  comentario_find = bs.find_all("div", class_='complaint-detail-body-description')
-  comentario_text = [c.get_text(' ', strip=True).strip() for c in comentario_find]
-
-  dictf[brand_title_text[lenght_brand+3:]] = ''.join(comentario_text)
-
-  # saves all the complaints to a csv file
-  with open('{}.csv'.format(brand), 'w+', encoding='latin-1', errors='replace') as a_file:
-    writer = csv.writer(a_file)
-    for key, value in dictf.items():
-      writer.writerow([key, value])
 
   # find a new link and iterate it over
   for link in bs.find_all('a', href=re.compile('^(https://portaldaqueixa.com/brands/{}/complaints/{}.*)'.format(brand, brand))):
@@ -46,4 +28,26 @@ def getLinks(brand):
         print('-'*20)
         print(newPage)
         pages.add(newPage)
-        getLinks(brand)
+        complaint = getInfo(brand, newPage)
+        complaints.append(complaint)
+  return complaints
+
+def getInfo(brand, url):
+  html = urlopen(url)
+  bs = BeautifulSoup(html, 'html.parser')
+
+  brand_title_find = bs.find('h4')
+  brand_title_text = (brand_title_find.get_text())[len(brand)+3:]
+
+  comentario_find = bs.find_all("div", class_='complaint-detail-body-description')
+  comentario_text = ''.join([c.get_text(' ', strip=True).strip() for c in comentario_find])
+
+  return (brand_title_text, comentario_text)
+
+def writeFile(brand, complaints):
+  with open('{}.csv'.format(brand), 'w+', encoding='latin-1', errors='replace') as a_file:
+    # saves all the complaints to a csv file
+    writer = csv.writer(a_file)
+    writer.writerow(['title', 'comment'])
+    for (title, comment) in complaints:
+      writer.writerow([title, comment])
