86fb25455f2b4031ffccf7857eea810cfe60b079
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-08-20 18:18:12 +0100

    app update

--- app/sql_conn.py
+++ app/sql_conn.py
@@ -1,35 +1,11 @@
 import pymysql
 import json
 import pandas as pd
-import os
-import sqlalchemy
 
-db_user = os.environ["DB_USER"]
-db_pass = os.environ["DB_PASS"]
-db_name = os.environ["DB_NAME"]
-db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
-cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
-
-pool = sqlalchemy.create_engine(
-    # Equivalent URL:
-    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
-    sqlalchemy.engine.url.URL.create(
-        drivername="mysql+pymysql",
-        username=db_user,  # e.g. "my-database-user"
-        password=db_pass,  # e.g. "my-database-password"
-        database=db_name,  # e.g. "my-database-name"
-        query={
-            "unix_socket": "{}/{}".format(
-                db_socket_dir,  # e.g. "/cloudsql"
-                cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
-        }
-    ),
-    **db_config
-)
 
 def sql_df(brand=None):
 
-    conn = pool.connect()
+    conn = pymysql.connect(host="34.78.88.69", user="root", db="scrapper")
     cur = conn.cursor()
 
     query = "SELECT * FROM {}".format(brand)
@@ -44,7 +20,7 @@ def sql_df(brand=None):
 
 def count_queixas(brand=None):
 
-    conn = pool.connect()
+    conn = pymysql.connect(host="34.78.88.69", user="root", db="scrapper")
     cur = conn.cursor()
 
     query = "SELECT COUNT(*) FROM {}".format(brand)
--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,11 +5,11 @@ export REGION=europe-west1
 export CONNECTION_NAME=portaldaqueixa-nlp:europe-west1:portaldaqueixa
 
 gcloud builds submit \
-  --tag gcr.io/$PROJECT_ID/portaldaqueixa \
+  --tag gcr.io/$PROJECT_ID/portaldaqueixa-app \
   --project $PROJECT_ID
 
-gcloud run deploy poll \
-  --image gcr.io/$PROJECT_ID/portaldaqueixa \
+gcloud run deploy portaldaqueixa-app \
+  --image gcr.io/$PROJECT_ID/portaldaqueixa-app \
   --platform managed \
   --region $REGION \
   --allow-unauthenticated \
