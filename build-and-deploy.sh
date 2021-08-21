2558562e1beccb92fcb9ad0da423cffa1c2c112f
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-08-21 08:29:05 +0100

    app update

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,15 +3,23 @@
 export PROJECT_ID=portaldaqueixa-nlp
 export REGION=europe-west1
 export CONNECTION_NAME=portaldaqueixa-nlp:europe-west1:portaldaqueixa
+export SERVICE_NAME=portaldaqueixa-app
 
 gcloud builds submit \
   --tag gcr.io/$PROJECT_ID/portaldaqueixa-app \
   --project $PROJECT_ID
 
-gcloud run deploy portaldaqueixa-app \
-  --image gcr.io/$PROJECT_ID/portaldaqueixa-app \
+gcloud run deploy $SERVICE_NAME \
+  --image gcr.io/$PROJECT_ID/$SERVICE_NAME \
   --platform managed \
   --region $REGION \
   --allow-unauthenticated \
   --add-cloudsql-instances $CONNECTION_NAME \
   --project $PROJECT_ID
+
+gcloud beta run services update SERVICE_NAME \
+  --add-cloudsql-instances $CONNECTION_NAME
+  --update-env-vars CLOUD_SQL_CONNECTION_NAME=$CONNECTION_NAME \
+  --update-env-vars DB_USER=root:latest \
+  --update-env-vars DB_PASS=secretpassword:latest \
+  --update-env-vars DB_NAME=scrapper:latest
