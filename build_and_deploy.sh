5163872a01a83d9a401da7299d7db53f48a1a0bf
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-08-21 17:03:45 +0100

    Create build_and_deploy.sh

---
+++ build_and_deploy.sh
@@ -0,0 +1,20 @@
+export PROJECT_ID=YOUR-PROJECT
+export REGION=YOUR-REGION
+export CONNECTION_NAME=YOUR-CONNECTION-NAME
+export SERVICE_NAME=CHOOSE-SERVICE-NAME
+export IMAGE=gcr.io/$PROJECT_ID/$SERVICE_NAME
+
+gcloud builds submit \
+  --tag $IMAGE \
+  --project $PROJECT_ID
+
+gcloud run deploy $SERVICE_NAME \
+  --image $IMAGE \
+  --project $PROJECT_ID \
+  --region $REGION \
+  --allow-unauthenticated \
+  --add-cloudsql-instances $CONNECTION_NAME \
+  --set-env-vars CLOUD_SQL_CONNECTION_NAME=$CONNECTION_NAME \
+  --set-env-vars DB_USER=user \
+  --set-env-vars DB_PASS=pass \
+  --set-env-vars DB_NAME=db
