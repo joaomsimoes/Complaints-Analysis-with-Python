fff3bdef2bd8e880a8951ce3fb99f9f006547155
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-08-21 08:30:11 +0100

    Update sql_conn.py

--- app/sql_conn.py
+++ app/sql_conn.py
@@ -1,11 +1,29 @@
-import pymysql
+import sqlalchemy
 import json
 import pandas as pd
 
+db_user = os.environ["DB_USER"]
+db_pass = os.environ["DB_PASS"]
+db_name = os.environ["DB_NAME"]
+db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
 
-def sql_df(brand=None):
+# https://cloud.google.com/sql/docs/mysql/connect-run - Connect Cloud Run to Cloud SQL
+pool = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL.create(
+        drivername="mysql+pymysql",
+        username=db_user,  # e.g. "my-database-user"
+        password=db_pass,  # e.g. "my-database-password"
+        database=db_name,  # e.g. "my-database-name"
+        query={"unix_socket": "/cloudsql/{}/".format(cloud_sql_connection_name)}),
+    **db_config
+)
 
-    conn = pymysql.connect(host="34.728.828.69", user="root", db="scrapper")
+def sql_df(brand=None, pool=pool):
+
+    conn = pool.connect()
     cur = conn.cursor()
 
     query = "SELECT * FROM {}".format(brand)
@@ -18,9 +36,9 @@ def sql_df(brand=None):
     return df
 
 
-def count_queixas(brand=None):
+def count_queixas(brand=None, pool=pool):
 
-    conn = pymysql.connect(host="34.7218.188.69", user="root", db="scrapper")
+    conn = pool.connect()
     cur = conn.cursor()
 
     query = "SELECT COUNT(*) FROM {}".format(brand)
