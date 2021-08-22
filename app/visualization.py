574cb6e4e79b8e2ace51b05e6496b1fdf879a48f
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-08-22 17:09:35 +0100

    Update visualization.py

--- app/visualization.py
+++ app/visualization.py
@@ -6,11 +6,20 @@ import matplotlib.pyplot as plt
 
 
 def display_topics(model, features, no_top_words=5):
+    dicts = {}
+    new_list = []
+    
     for topic, word_vector in enumerate(model.components_):
         largest = word_vector.argsort()[::-1]
-        st.subheader('\nTopic {}'.format(topic))
-        for i in range(0, no_top_words):
-            st.text('{}'.format(features[largest[i]]))
+        dicts["Tópico " + str(topic+1)] = new_list
+        
+        for i in range(0, 5):
+          new_list.append(features[largest[i]])
+          if i == 4:
+            new_list = []
+    
+    df_topicos = pd.DataFrame.from_dict(dicts)
+    st.write(df_topicos)
 
 
 def word_cloud(word_freq, title=None, max_words=200):
